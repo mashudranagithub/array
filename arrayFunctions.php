1c727da7ec03f2c1a4a15e1cd826f319dd550076
mashud <mashudranabitm@gmail.com>
2017-07-24 17:30:00 +0600

    slice & case change practice complete

--- arrayFunctions.php
+++ arrayFunctions.php
@@ -75,7 +75,59 @@ print_r(array_chunk($second_array, 3, true));
 
 $slicekorbo=array('sjdakjf','dsjifhewj', 'huvhru', 'vnurh', 'rvhrribv');
 
-print_r($slicecomplete = array_slice($slicekorbo, 2));
+print_r($slicecomplete = array_slice($slicekorbo, 2, -2));
+
+
+
+
+
+$slice = array(
+    'ab',
+    'ae',
+    'ar',
+    'ay',
+    'at',
+    'dd',
+    'dds'
+);
+
+
+print_r(array_slice($slice,3,-1));
+
+
+
+
+//array_change_key_case() Practice
+
+
+$food = array(
+    'Apple'=>'Juice',
+    'Cucumber'=>'Salat',
+    'Egg'=>'Mumlet',
+    'fish'=>'curry',
+    'meat'=>'Kalavuna',
+    'banana'=>'juice',
+    'mango'=>'juice',
+    'pinapple'=>'juice'
+    );
+
+
+$foodOne = array(
+    'APPLE'=>'Juice',
+    'CUCUMEBR'=>'Salat',
+    'EGG'=>'Mumlet',
+    'FISH'=>'curry',
+    'MEAT'=>'Kalavuna',
+    'BANANA'=>'juice',
+    'MANGO'=>'juice',
+    'PINEAPPLE'=>'juice'
+);
+
+
+print_r(array_change_key_case($food,CASE_UPPER));
+
+print_r(array_change_key_case($food,CASE_LOWER));
+
 
 
 ?>
