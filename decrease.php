764b2f4eb1ffcbc1a265af7cf5b388bede218f4c
mashud <mashudranabitm@gmail.com>
2017-07-25 00:14:28 +0600

    aro kiso kaj korlam

---
+++ decrease.php
@@ -0,0 +1,26 @@
+<?php
+
+
+echo "<pre>";
+
+$mesMember = array(
+    'Khaled Vai',
+    'Enaan',
+    'Rahim',
+    'Samian',
+    'Mustafiz',
+    'Saifur',
+    'Mashud'
+);
+
+//print_r($mesMember);
+
+//array_pop($mesMember);
+
+print_r($mesMember);
+
+
+print_r(array_pad($mesMember,11,"blue"));
+
+
+?>
